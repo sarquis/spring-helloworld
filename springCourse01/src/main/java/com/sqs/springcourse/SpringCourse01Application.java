35884564d1fd8ee5a71e8b18c73bd5971ba49c75
rsnf <roberto.sarquis@gmail.com>
2023-07-02 14:58:45 -0300

    first commit

---
+++ springCourse01/src/main/java/com/sqs/springcourse/SpringCourse01Application.java
@@ -0,0 +1,13 @@
+package com.sqs.springcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCourse01Application {
+
+    public static void main(String[] args) {
+	SpringApplication.run(SpringCourse01Application.class, args);
+    }
+
+}
---
+++ springCourse01/src/test/java/com/sqs/springcourse/SpringCourse01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqs.springcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCourse01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
